7916b939b32cfadb7073e2143430f720f602b76e
Valerio Casadei <valeriocasadei18@gmail.com>
2024-09-24 18:50:51 +0200

    Rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,40 @@
 import "./App.css";
+import "boxicons";
 
 function App() {
 	return (
 		<>
-			<div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
-				<div className="py-0 md:py-20 flex flex-col md:flex-row justify-center gap-5 h-[100vh]">
-					<div className="basis-full md:basis-1/2 flex flex-col justify-between">
-						<div>
-							<div>
-								<h1 className="orange text-5xl font-bold mb-2">Valerio Casadei</h1>
-								<h2 className="white text-3xl mb-2">Junior Frontend Developer</h2>
-								<p className="max-w-xs">I like to build responsive and accessible interfaces.</p>
-							</div>
-						</div>
-						<div>
-							<p>ciao</p>
-						</div>
-					</div>
-					<main className="basis-full md:basis-1/2">
-						<section>
-							<span className="font-bold">Navigate to...</span>
-							<ul className="flex flex-row justify-between mt-5">
-								<li>
-									<a href="#about" className="navButton">
-										ABOUT ME
-									</a>
-								</li>
-								<li>
-									<a href="#experience" className="navButton">
-										EXPERIENCE
-									</a>
-								</li>
-								<li>
-									<a href="#project" className="navButton">
-										PROJECT
-									</a>
-								</li>
-							</ul>
-						</section>
-						<section id="about" className="mt-10">
-							<h3 className="font-bold white py-5">ABOUT ME</h3>
-							<p className="pb-5">
-								Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi facere error est dignissimos eaque
-								vitae eum labore voluptates! Minus nobis qui beatae necessitatibus tempore architecto asperiores, hic
-								pariatur inventore molestias obcaecati, explicabo ad ipsum, doloribus totam at adipisci ducimus
-								aspernatur.
-							</p>
-							<p>Qui scrivo qualcosa su di me</p>
-						</section>
-						<section id="experience" className="mt-10">
-							<h3 className="font-bold white py-5">EXPERIENCE</h3>
-							<ol>
-								<li className="card mb-3 px-1 py-3 md:p-3 flex flex-col md:flex-row">
-									<div className="basis-full md:basis-1/3">
-										<p>10/2023 - 03/2024</p>
-									</div>
-									<div className="basis-full md:basis-2/3">
-										<h3 className="white font-bold">Frontend Developer Course • EPICODE</h3>
-										<p>descrivo epicode</p>
-									</div>
-								</li>
-								<li className="card mb-3 px-1 py-3 md:p-3 flex flex-col md:flex-row">
-									<div className="basis-full md:basis-1/3">
-										<p>10/2023 - 03/2024</p>
-									</div>
-									<div className="basis-full md:basis-2/3">
-										<h3 className="white font-bold">Frontend Developer Course • EPICODE</h3>
-										<p>descrivo epicode</p>
-									</div>
-								</li>
-							</ol>
-						</section>
-						<section id="#project" className="mt-10">
-							<h3 className="font-bold white py-5">PROJECT</h3>
-						</section>
-					</main>
+			<div className="mx-auto min-h-screen max-w-screen-xl my-5 flex flex-col">
+				<div className="myNav mx-2 p-3 mb-5">
+					<ul className="flex flex-row justify-center gap-5">
+						<li>About Me</li>
+						<li>Experience</li>
+						<li>Projects</li>
+						<li>Contact</li>
+					</ul>
 				</div>
+				<main id="about" className="mx-7 flex flex-col md:items-center">
+					<div className="font-bold space-y-1 mb-5">
+						<h2 className="text-3xl">Hello! I am</h2>
+						<h1 className="text-5xl">Valerio Casadei</h1>
+						<h3 className="text-2xl">
+							and I am a <span className="orange">Front-end Web Developer</span>
+						</h3>{" "}
+					</div>
+					<div className="flex flex-row gap-5 mb-5">
+						<a href="https://www.linkedin.com/in/valerio-casadei-frontenddeveloper/" target="_blank">
+							<i className="bx bxl-linkedin myIcon"></i>
+						</a>
+						<a href="https://github.com/ValCas18" target="_blank">
+							<i className="bx bxl-github myIcon"></i>
+						</a>
+					</div>
+					<div>
+						<a href="./assets/[ITA]CV_Casadei_Valerio1.pdf" download>
+							<button className="dlBtn px-6 py-3 bg-[#fd7014] text-black rounded-full mb-5">Download CV</button>
+						</a>
+					</div>
+				</main>
 			</div>
 		</>
 	);
