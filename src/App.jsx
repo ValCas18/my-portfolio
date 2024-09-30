17f29c4b35acc525ce3627076ecbf354779bfab3
Valerio Casadei <valeriocasadei18@gmail.com>
2024-09-30 18:25:40 +0200

    projects section wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { faLaptopCode, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faLaptopCode, faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import "./App.css";
+import wowImg from "./assets/wow.png";
+import postitImg from "./assets/postit.png";
+import galleryImg from "./assets/gallery.png";
+import weatherImg from "./assets/weather.png";
+import spotifyImg from "./assets/spotify.png";
+import quizImg from "./assets/quiz.png";
 
 function App() {
 	return (
 		<>
-			<div className="mx-auto mt-5 max-w-screen-xl flex flex-col md:min-h-screen absolute">
+			<div className="mx-auto mt-5 max-w-screen-xl flex flex-col md:min-h-screen">
 				<div className="myNav mx-2 p-3 mb-5 sticky top-0">
 					<ul className="flex flex-row justify-center gap-5">
 						<li className="navItem" href="#about">
@@ -92,15 +98,67 @@ function App() {
 					</div>
 				</section>
 				<section id="project" className="bg-[#323946] px-7 py-14">
-					<div>
-						<h2 className="text-3xl font-extrabold text-center">
-							My <span className="orange">Projects</span>
-						</h2>
-						<p className="my-5">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit sapiente ducimus earum quaerat at maiores
-							nesciunt nulla commodi quas ipsam.
-						</p>
-						<p className="my-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum, soluta.</p>
+					<div className="flex flex-col gap-10">
+						<div className="relative group">
+							<img src={galleryImg} alt="gallery project" className="basis-full rounded-md" />{" "}
+							<div className="px-3 text-center absolute bottom-0 left-0 right-0 bg-gradient-to-t from-orange-500 to-transparent flex flex-col items-center justify-center text-white h-0 opacity-0 transition-all duration-500 ease-in-out group-hover:h-full group-hover:opacity-100 rounded-md">
+								<h2 className="text-3xl font-bold">My Gallery</h2>
+								<p className="mt-2 overflow-hidden max-h-24">Images showcase app. API Fetch and Tailwind practice.</p>
+								<a href="https://github.com/ValCas18/mygallery-app" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faArrowRight} className="mt-5 border border-2 rounded-full p-2" />
+								</a>
+							</div>
+						</div>
+						<div className="relative group">
+							<img src={postitImg} alt="post-it project" className="basis-full rounded-md" />
+							<div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-orange-500 to-transparent flex flex-col items-center justify-center text-white h-0 opacity-0 transition-all duration-500 ease-in-out group-hover:h-full group-hover:opacity-100 rounded-md">
+								<h2 className="text-3xl font-bold">Gallery Project</h2>
+								<p className="mt-2">A showcase of images with hover effects</p>
+								<a href="https://github.com/ValCas18/mygallery-app" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faArrowRight} className="mt-5 border border-2 rounded-full p-2" />
+								</a>
+							</div>
+						</div>
+						<div className="relative group">
+							<img src={weatherImg} alt="weather app project" className="basis-full rounded-md" />{" "}
+							<div className="px-3 text-center absolute bottom-0 left-0 right-0 bg-gradient-to-t from-orange-500 to-transparent flex flex-col items-center justify-center text-white h-0 opacity-0 transition-all duration-500 ease-in-out group-hover:h-full group-hover:opacity-100 rounded-md">
+								<h2 className="text-3xl font-bold">My Gallery</h2>
+								<p className="mt-2 overflow-hidden max-h-24">Images showcase app. API Fetch and Tailwind practice.</p>
+								<a href="https://github.com/ValCas18/mygallery-app" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faArrowRight} className="mt-5 border border-2 rounded-full p-2" />
+								</a>
+							</div>
+						</div>
+						<div className="relative group">
+							<img src={wowImg} alt="wow character finder project" className="basis-full rounded-md" />
+							<div className="px-3 text-center absolute bottom-0 left-0 right-0 bg-gradient-to-t from-orange-500 to-transparent flex flex-col items-center justify-center text-white h-0 opacity-0 transition-all duration-500 ease-in-out group-hover:h-full group-hover:opacity-100 rounded-md">
+								<h2 className="text-3xl font-bold">My Gallery</h2>
+								<p className="mt-2 overflow-hidden max-h-24">Images showcase app. API Fetch and Tailwind practice.</p>
+								<a href="https://github.com/ValCas18/mygallery-app" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faArrowRight} className="mt-5 border border-2 rounded-full p-2" />
+								</a>
+							</div>
+						</div>
+						<div className="relative group">
+							<img src={spotifyImg} alt="spotify clone project" className="basis-full rounded-md" />
+							<div className="px-3 text-center absolute bottom-0 left-0 right-0 bg-gradient-to-t from-orange-500 to-transparent flex flex-col items-center justify-center text-white h-0 opacity-0 transition-all duration-500 ease-in-out group-hover:h-full group-hover:opacity-100 rounded-md">
+								<h2 className="text-3xl font-bold">My Gallery</h2>
+								<p className="mt-2 overflow-hidden max-h-24">Images showcase app. API Fetch and Tailwind practice.</p>
+								<a href="https://github.com/ValCas18/mygallery-app" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faArrowRight} className="mt-5 border border-2 rounded-full p-2" />
+								</a>
+							</div>
+						</div>
+						<div className="relative group">
+							<img src={quizImg} alt="quiz project" className="basis-full rounded-md" />
+							<div className="px-3 text-center absolute bottom-0 left-0 right-0 bg-gradient-to-t from-orange-500 to-transparent flex flex-col items-center justify-center text-white h-0 opacity-0 transition-all duration-500 ease-in-out group-hover:h-full group-hover:opacity-100 rounded-md">
+								<h2 className="text-3xl font-bold">My Gallery</h2>
+								<p className="mt-2 overflow-hidden max-h-24">Images showcase app. API Fetch and Tailwind practice.</p>
+								<a href="https://github.com/ValCas18/mygallery-app" target="_blank" rel="noopener noreferrer">
+									<FontAwesomeIcon icon={faArrowRight} className="mt-5 border border-2 rounded-full p-2" />
+								</a>
+							</div>
+						</div>
 					</div>
 				</section>
 				<section id="contact" className="px-7 py-14">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				expandUp: {
+					"0%": { height: "0%", opacity: "0" },
+					"100%": { height: "100%", opacity: "1" },
+				},
+			},
+			animation: {
+				"expand-up": "expandUp 0.5s ease-in-out forwards",
+			},
+		},
 	},
 	plugins: [],
 };
